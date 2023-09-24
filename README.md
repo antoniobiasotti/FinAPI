aa42b176e7f7ecb3192e56eb6b965c5639dfabe9
Antonio <abamorim36@gmail.com>
2023-09-24 15:31:55 -0300

    FinAPI

---
+++ README.md
@@ -0,0 +1,19 @@
+### Requisitos
+
+- [ X ] Deve ser possível criar uma conta
+- [ X ] Deve ser possível buscar o extrato bancário do cliente
+- [ X ] Deve ser possível realizar um depósito
+- [] Deve ser possível realizar um saque
+- [] Deve ser possível buscar o extrato do cliente por data
+- [] Deve ser possível atualizar dados da conto do cliente
+- [] Deve ser possível obter dados da conta do cliente
+- [] Deve ser possível deletar uma conta
+
+### Regras de Negócio
+
+- [ X ] Não deve ser possível cadastrar uma conta com CPF já existente
+- [ X ] Não deve ser possível fazer depósito em uma conta não existente
+- [ X ] Não deve ser possível buscar extrato em uma conta não existente
+- [] Não deve ser possível fazer saque em uma conta não existente
+- [] Não deve ser possível excluir uma conta não existente
+- [] Não deve ser possível fazer saque quando o saldo for insuficiente
---
+++ src/index.js
@@ -0,0 +1,122 @@
+const express = require('express')
+const { v4: uuidv4 } = require('uuid')
+
+const app = express();
+
+app.use(express.json());
+
+const customers = []; //Array
+
+// Middleware
+
+function verifyIfExistsAccountCPF(req, res, next) {
+     const { cpf } = req.headers;
+
+     const customer = customers.find(
+          (customer) => customer.cpf === cpf
+     );
+
+     if(!customer) {
+          return res.status(400).json({ error: 'Customer not found' });
+     };
+
+     req.customer = customer;
+
+     return next();
+}
+
+function getBalance(statement){
+     const balance = statement.reduce((acc, operation) => {
+          if(operation.type === 'credit'){
+               return acc + operation.amount;
+          }else {
+               return acc - operation.amount;
+          }
+     }, 0);
+
+     return balance;
+}
+
+app.get('/admin', (req, res) => {
+     return res.json(customers);
+})
+
+/**
+ * cpf - string
+ * name - string
+ * id - uuid (Identificador Único Universal)
+ * statement - [] 
+ */
+
+app.post('/account', (request, response) => {
+     const { cpf, name } = request.body;
+
+     const customerAlreadyExists = customers.some(
+          (customers) => customers.cpf === cpf
+     );
+
+     if(customerAlreadyExists) {
+          return response.status(400).json({ error: "Customer already exists!" })
+     }
+
+     customers.push({
+          cpf,
+          name,
+          id: uuidv4(),
+          statement: []
+     })
+
+     return response.status(201).send();
+});
+
+app.get('/statement', verifyIfExistsAccountCPF, (request, response) => {
+     const { customer } = request;
+
+     return response.json(customer.statement);
+});
+
+app.post('/deposit', verifyIfExistsAccountCPF, (request, response) => {
+     const { description, amount } = request.body;
+
+     const { customer } = request;
+
+     const statementOperation = {
+          description,
+          amount,
+          created_at: new Date(),
+          type: "credit",
+     }
+
+     customer.statement.push(statementOperation);
+
+     return response.status(201).send();
+})
+
+app.post('/withdraw', verifyIfExistsAccountCPF, (request, response) => {
+     const { amount } = request.body;
+     const { customer } = request;
+
+     const balance = getBalance(customer.statement);
+
+     if(balance < amount) {
+          return response.status(400).json({error: "Insufficient funds!"})
+     }
+
+     const statementOperation = {
+          amount,
+          created_at: new Date(),
+          type: "debit",
+     };
+
+     customer.statement.push(statementOperation);
+
+     return response.status(201).send();
+})
+
+app.get('/statement/data', verifyIfExistsAccountCPF, (request, response) => {
+     const { customer } = request;
+
+     return response.json(customer.statement);
+});
+
+app.listen(3333)
